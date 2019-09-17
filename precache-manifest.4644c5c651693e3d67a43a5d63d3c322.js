2ff699ef8acd940272eb2676a30be2c1255c82d5
jeromecohen <jeromecohen98@gmail.com>
2019-09-17 09:32:09 -0400

    Updates

---
+++ precache-manifest.4644c5c651693e3d67a43a5d63d3c322.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a00e1610e23f86dcf864a328891d851e",
+    "url": "/fortnite/index.html"
+  },
+  {
+    "revision": "6d660a1f59ff91cc43fa",
+    "url": "/fortnite/static/css/main.439b0a9f.chunk.css"
+  },
+  {
+    "revision": "93a3c26265d03fd61f0f",
+    "url": "/fortnite/static/js/2.3f5ac5cb.chunk.js"
+  },
+  {
+    "revision": "6d660a1f59ff91cc43fa",
+    "url": "/fortnite/static/js/main.1550ca79.chunk.js"
+  },
+  {
+    "revision": "4585940433e5ed7fa57c",
+    "url": "/fortnite/static/js/runtime~main.0eedd327.js"
+  }
+]);
