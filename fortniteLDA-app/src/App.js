a41a2520011c02674ecbb9de7fc8580f3eb64714
jeromecohen <jeromecohen98@gmail.com>
2019-09-17 16:42:36 -0400

    commment spelling

--- fortniteLDA-app/src/App.js
+++ fortniteLDA-app/src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 import './App.css';
 import { makeStyles } from '@material-ui/core/styles';
 
-//Material UI Componets 
+//Material UI Components 
 import Button from '@material-ui/core/Button';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
